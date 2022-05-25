8abef6ec33cfe186eedba21ec83493de7fa55bc5
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2022-05-25 11:27:20 +0200

    updating the purchase confirmation in the form

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+// Disabling the "submit" action to avoid submitting the form //
+
+document.getElementById("book").addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
+// Creating and styling a "purchase confirmation" message //
+
 document.getElementById("book").onclick = function () {
   document.getElementById("book").innerHTML =
-    "✅ Purchase successful! Check your email for confirmation";
-  document.getElementById("book").style.width = "400px";
-  document.getElementById("book").style.height = "100px";
-  document.getElementById("book").style.background = "blue";
+    "Lines reserved successfully! Check your email for confirmation ✅";
+  document.getElementById("book").style.width = "410px";
+  document.getElementById("book").style.height = "110px";
+  document.getElementById("book").style.background = "#006BBB";
+  document.getElementById("book").style.borderColor = "white";
   document.getElementById("book").style.color = "white";
+  document.getElementById("book").style.fontSize = "25px";
   document.getElementById("booking-text").style.display = "none";
   document.getElementById("email").style.display = "none";
   document.getElementById("number-of-guests").style.display = "none";
   document.getElementById("text").style.display = "none";
   document.getElementById("date").style.display = "none";
   document.getElementById("time").style.display = "none";
+  document.getElementById("return").style.display = "initial";
 
-  function timedRefresh(timeoutPeriod) {
+  // Return button will refresh the website //
+
+  document.getElementById("return").onclick = function timedRefresh(
+    timeoutPeriod
+  ) {
     setTimeout("location.reload(true);", timeoutPeriod);
-  }
-  window.onload = timedRefresh(4000);
+  };
+  window.onload = timedRefresh(1000);
 };
